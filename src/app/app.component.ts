e24fddbbfdb392ece103e4af240ec16d16aef29a
makhmadane96 <makhmadane.lo@jmadsolutions.sn>
2025-07-20 13:47:35 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_iage_km_2';
+
+  alert(){
+    alert("je suis ....");
+  }
 }
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent implements OnInit {
+
+  products: any = [];
+
+  //IOC INJECTION DE DEPENCDANCE
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  //Constructeur 
+  ngOnInit(): void {
+      this.getAllProducts();
+
+  }
+
+  getAllProducts() {
+    this.httpClient.get("http://localhost:8000/api/products").subscribe(
+      (data: any) => {
+        this.products = data.products;
+        console.log(this.products)
+      },
+      () => {
+        console.log("error")
+      }
+    )
+  }
+}
